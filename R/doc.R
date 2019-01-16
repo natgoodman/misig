3ae9c894ca28ec0c4b741e0b31318096931f2407
natgoodman <natg@shore.net>
2019-01-16 10:50:21 -0800

    Rewrite using paramaters in param environment instead of globals

---
+++ R/doc.R
@@ -0,0 +1,210 @@
+#################################################################################
+##
+## Author:  Nat Goodman
+## Created: 19-01-10
+##          from repwr/doc_resig.R created 18-06-19
+##
+## Copyright (C) 2019 Nat Goodman.
+## 
+## General functions to generate figures and tables for documents
+##
+## This software is open source, distributed under the MIT License. See LICENSE
+## file at https://github.com/natgoodman/NewPro/FDR/LICENSE 
+##
+#################################################################################
+## --- Generate Figures and Tables for Document ---
+## sect is which sections to run - for use during development
+##   uses prefix matching and all matches run
+## other params passed to init or init_doc. typically
+##   subdoc - NULL or 'supp'. default: NULL
+##   fignum, tblnum - initial figure, table number. default: 1
+##   figpfx, tblpfx - prefix prepended to figure, table number, eg, 'S' for supp
+##   figsfx, tblsfx - suffices appended to figure, table number in 'blocks' eg, a,b,c,...
+##   save.out - save figures, tables to files. default: T
+##   figscreen - plot figures on screen. default: T for readme, F otherwsie
+##   fignew - plot each figure in new window. default figscreen
+##   docfun document-specific function. default calculated from doc,subdoc eg, doc_repwr
+dodoc=
+  function(sect=NULL,need.init=T,xperiment=F,...) {
+    if (!xperiment) {
+      ## normal doc
+      if (need.init) wrap_fun(init,...);
+      wrap_fun(init_doc,...);
+    } else {
+      ## experimenal sandbox
+      if (need.init) wrap_fun(init_xperiment,...);
+      wrap_fun(init_doc_xperiment,...);
+    }
+    param(docfun);
+    docfun(sect=sect);
+  }
+
+## --- Document Functions ---
+## utility functions to make figures and tables for documents
+## run plot function, save if required, label result with name
+## title -- can be NULL, string, or function -- if function, call to get actual title
+##   needed to generate correct titles for extra figures
+## extra -- figure is 'extra' - will not be included in document
+## CAUTION: ... interacts with partial argument matching to cause dofig args to be
+##   matched by plot-function args. eg, 'd' matches 'doc', 'x' matches 'xtra'
+##   choose argument names carefully!
+dofig=
+  function(figfun,figname=NULL,title=NULL,extra=parent(extra,F),sect=parent(sect,NULL),...) {
+    param(figextra,save.fig,figscreen,fignew);
+    if (extra&!figextra) return();
+    file=filename_fig(figlabel(where='filename'),sect,figname);
+    plot.to.file=((is.na(save.fig)&!file.exists(file))|(!is.na(save.fig)&save.fig));
+    plot.to.screen=figscreen;           # for stylistic consistency
+    ## NG 18-08-10: new scheme for plotting to file
+    ##   plot to screen and file: dev.new here, dev.copy later
+    ##   plot to screen only: dev.new here, no dev.copy later
+    ##   plot to file only: png here, dev.off later
+    ## plot.to.file only doesn't work if figfun returns multiple figures
+    ##   trash multi-figure capability. we don't use it now
+    if (!(plot.to.file||plot.to.screen)) {
+      msg=paste(sep=' ',paste_nv(figscreen),'and',paste_nv(save.fig));
+      if (is.na(save.fig)) msg=paste(sep=' ',msg,'and figure file',file,'exists');
+      msg=paste(sep=' ',msg,'which means there is no where to plot the figure');
+      stop(msg);
+    }
+    ##   bg='white' needed else image copied with transparent bg; renders as grey
+    if (plot.to.screen) {
+      dev=dev.new(bg='white');
+      dev=dev.cur();
+    }
+    if (plot.to.file&!plot.to.screen) {
+      ## png parameters found by trial and error. look reasonable
+      ## TODO: learn the right way to do this!
+      png(filename=file,height=8,width=8,units='in',res=200,pointsize=12);
+      dev.png=dev.cur();
+    }
+    if (is.function(title)) title=wrap_fun(title,...);
+    ## if (is.null(title)) fignum=figname(name,sect,sectnum);  # set fignum for standard plot title
+    ## draw the figure!
+    ## wrap_fun(figfun,...);
+    figfun(title=title,...);
+    if (plot.to.file&plot.to.screen) 
+      ## png parameters found by trial and error. look reasonable
+      ## TODO: learn the right way to do this!
+      dev.png=dev.copy(png,filename=file,height=8,width=8,units='in',res=200,pointsize=12);
+    ## always close plot.to.file device
+    if (plot.to.file)  dev.off(dev.png);
+    ## close plot.to.screen device unless user wants each figure in new window
+    if (plot.to.screen&&!fignew) dev.off(dev);
+    figinc();
+    figname;
+  }
+## save one or more tables.
+dotbl=
+  function(...,sect=parent(sect,NULL),list=character()) {
+    dots=match.call(expand.dots=FALSE)$...;  # doesn't evaluate dots
+    parent.env=parent.frame(n=1);            # for empty name
+    if (length(dots) &&
+        !all(vapply(dots,function(x) is.atomic(x)||is.symbol(x)||is.character(x),
+                    NA,USE.NAMES=FALSE))) 
+      stop("... must contain atomic data like names or character strings");
+    tblname=vapply(dots,as.character,"");
+    if (length(tblname)==0L) tblname=character();
+    tblname=c(list,tblname);
+    ## make sure all table names valid
+    bad=which(sapply(tblname,function(tblname) !exists(tblname,envir=parent.env)));
+    if (any(bad)) stop(paste(sep=' ','Invalid table names(s):',paste(collapse=', ',names(bad))))
+    sapply(tblname,function(tblname) {
+      tbl=get(tblname,envir=parent.env);
+      file=filename_tbl(tbllabel(where='filename'),sect,tblname);
+      save_tbl(tbl,file);
+      tblinc()});
+    tblname;
+ }
+## construct figure label, eg, S1-2c
+figlabel=function(extra=parent(extra,F),where=cq(content,filename)) {
+  where=match.arg(where);
+  if (extra) {
+    param(xfigpfx,sectnum,xfignum,xfigsfx,xfigblk);
+    pfx=xfigpfx;
+    sfx=if(!is.null(xfigblk)) xfigsfx[xfigblk] else NULL;
+    num=xfignum;
+  } else {
+    param(figpfx,sectnum,fignum,figsfx,figblk);
+    pfx=figpfx;
+    sfx=if(!is.null(figblk)) figsfx[figblk] else NULL;
+    num=fignum;
+  }
+  if (where=='filename') {
+    num=sprintf('%03i',num);
+    if (!is.null(sectnum)) sectnum=sprintf('%02i',sectnum);
+  }
+  pfx=if(!is.null(sectnum)) paste(collapse='',c(pfx,sectnum)) else pfx;
+  paste(collapse='-',c(pfx,paste(collapse='',c(num,sfx))));
+}
+## construct table label, eg, S1-2c
+tbllabel=function(where=cq(content,filename)) {
+  where=match.arg(where);
+  param(tblpfx,sectnum,tblnum,tblsfx,tblblk);
+  if (where=='filename') {
+    tblnum=sprintf('%03i',tblnum);
+    if (!is.null(sectnum)) sectnum=sprintf('%02i',sectnum);
+  }
+  pfx=if(!is.null(sectnum)) paste(collapse='',c(tblpfx,sectnum)) else tblpfx;
+  sfx=if(!is.null(tblblk)) tblsfx[tblblk] else NULL;
+  paste(collapse='-',c(pfx,paste(collapse='',c(tblnum,sfx))));
+}
+## manage figure,table numbers, blocks
+figinc=function(extra=parent(extra,F))
+  if (extra) {
+    param(xfigblk,xfignum);
+    if (!is.null(xfigblk)) param(xfigblk=xfigblk+1) else param(xfignum=xfignum+1);
+  } else {
+    param(figblk,fignum);
+    if (!is.null(figblk)) param(figblk=figblk+1) else param(fignum=fignum+1);
+  }
+figblk_start=function(extra=parent(extra,F)) {
+  if (extra) return(xfigblk_start());
+  param(figblk,fignum);
+  ## if already in block, end it
+  if (!is.null(figblk)) param(fignum=fignum+1);
+  param(figblk=1);
+}
+figblk_end=function(extra=parent(extra,F)) {
+  if (extra) {xfigblk_end(); return(); }
+  param(figblk,fignum);
+  ## do nothing if not in block, else end it
+  if (!is.null(figblk)) param(figblk=NULL,fignum=fignum+1);
+}
+xfigblk_start=function() {
+  param(xfigblk,xfignum);
+  ## if already in block, end it
+  if (!is.null(xfigblk)) param(xfignum=xfignum+1);
+  param(xfigblk=1);
+}
+xfigblk_end=function() {
+  param(xfigblk,xfignum);
+  ## do nothing if not in block, else end it
+  if (!is.null(xfigblk)) param(xfigblk=NULL,xfignum=xfignum+1);
+}
+tblinc=function() {
+    param(tblblk,tblnum);
+    if (!is.null(tblblk)) param(tblblk=tblblk+1) else param(tblnum=tblnum+1);
+}
+tblblk_start=function() {
+  param(tblblk,tblnum);
+  ## if already in block, end it
+  if (!is.null(tblblk)) param(tblnum=tblnum+1);
+  param(tblblk=1);
+}
+tblblk_end=function() {
+  param(tblblk,tblnum);
+  ## do nothing if not in block, else end it
+  if (!is.null(tblblk)) param(tblblk=NULL,tblnum=tblnum+1);
+}
+outblk_start=function() {
+  figblk_start(extra=F);
+  xfigblk_start();
+  tblblk_start();
+}
+outblk_end=function() {
+  figblk_end(extra=F);
+  xfigblk_end();
+  tblblk_end();
+}
+
