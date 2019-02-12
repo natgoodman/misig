b1feb88da768cafb802885d05210d6999740f1ef
natgoodman <natg@shore.net>
2019-02-11 17:11:23 -0800

    Second version of blog post

--- R/doc_ovrfx.R
+++ R/doc_ovrfx.R
@@ -18,124 +18,104 @@
 ## no sections. only 2 figures
 ## n.fig is sample size for which figures plotted
 doc_ovrfx=function(n.fig1=20,d.fig1=0.3,sect=parent(sect,NULL)) {
+  param(n.meand,d0.meand);
+  meand=get_meand_empi();
+  meand.byd=split(meand,meand$d0);
+  ## make splines that interpolate meand
+  ##   empirically determined smooth.spline as interp function, and spar=0.3
+  spar=0.3;
+  meand.fit=lapply(meand.byd,function(meand) with(meand,smooth.spline(n,meand,spar=spar)))
+  over.fit=lapply(meand.byd,function(meand) with(meand,smooth.spline(n,over,spar=spar)))
   ## support statements in text or drawn on figures
   ## do it upfront 'cuz some used in figures
-  ##   d_crit(n=20)=0.64
-  d.crit1=d_crit(n.fig1);
-  ## compute/get data for plotting critical and mean observed effect sizes
-  meand=get_data(meand);
-  meand=subset(meand,subset=d0%in%c(0.3,0.5,0.7));
-  ## TEMPORARY until I fix domeand
-  meand=subset(meand,subset=n>=20);
-  ##
-  n=sort(unique(meand$n));
-  d.crit=d_crit(n);
-  ##  mean observed significant effect size
-  d.mean3=meand[meand$d0==0.3,];
-  d.mean5=meand[meand$d0==0.5,];
-  ##  mean observed significant effect size for n=20
-  d.mean3.20=d.mean3[d.mean3$n==n.fig1,'meand'];
-  d.mean5.20=d.mean5[d.mean5$n==n.fig1,'meand'];
-  ##  n that achieves d.crit=d.pop
-  n.crit3=uniroot(function(n) d2pval(n=n,d=0.3)-param(sig.level),interval=c(10,1000))$root;
-  n.crit5=uniroot(function(n) d2pval(n=n,d=0.5)-param(sig.level),interval=c(10,1000))$root;
-  ##  need n=122 for d=0.3 to achieve over=1.25, n=45 for d=0.5
-  n.over3=n2over(meand,d0=0.3);
-  n.over5=n2over(meand,d0=0.5);
+  ## d_crit(n=20)=0.64
+  d.crit1=d_crit(n.fig1); 
+  ## mean observed significant effect size and overestimate for n=20
+  ##   d=0.3, mean=0.81, over=2.7x
+  ##   d=0.5, mean=0.86, over=1.7x
+  ##   d=0.7, mean=0.93, over=1.3x
+  meand20=sapply(meand.fit,function(meand.fit) predict(meand.fit,20)$y);
+  over20=sapply(over.fit,function(over.fit) predict(over.fit,20)$y);
+  ## n that achieves over=1.25x
+  ##   d=0.3 n=121
+  ##   d=0.5 n=47
+  ##   d=0.7 n=26
+  nover=sapply(over.fit,function(over.fit) {
+    n2over=function(n) predict(over.fit,n)$y;
+    uniroot(function(n) n2over(n)-1.25,interval=c(10,200))$root});
   ## save supporting values in table
-  support=data.frame(
-    ##   d_crit(n=20)=0.64. computed above
-    d.crit=d.crit1,
-    ##   for n=20, d=0.3, average=0.82, overestimate of 2.7x
-    ##   for n=20, d=0.5, average=0.87, overestimate of 1.7x
-    d.mean3=d.mean3.20,
-    d.mean5=d.mean5.20,
-    d.over3=d.mean3[d.mean3$n==n.fig1,'over'],
-    d.over5=d.mean5[d.mean5$n==n.fig1,'over'],
-    ##  to achieve 1.25x, for d=0.3, n=122, for d=0.5, n=46
-    n.over3=n.over3,
-    n.over5=n.over5,
-    ##   need n=87 for d=0.3 to be significant, n=32 for d=0.5
-    n.crit3=n.crit3,
-    n.crit5=n.crit5);
+  names(meand20)=paste(sep='_','meand20',names(meand20));
+  names(over20)=paste(sep='_','over20',names(over20));
+  names(nover)=paste(sep='_','nover',names(nover));
+  support=data.frame(d.crit1,t(meand20),t(over20),t(nover));
   dotbl(support);
 
   ## draw the figures
   ## figure 1
-  title=title_ovrfx('P-values improve as observed effect size grows more extreme');
+  title=title_ovrfx('P-values improve as observed effect size grows more extreme',n=n.fig1);
   x='d.sdz'; y='d.pop';
-  sim=get_sim(n=n.fig1);
-  sim.pos_dsdz=subset(sim,subset=d.sdz>=0);
-  dofig(plotdvsd,'big_picture',sim=sim.pos_dsdz,x=x,y=y,d.crit=d.crit1,d.pop=d.fig1,xlim=c(0,2),
+  sim=get_sim_rand(n=n.fig1);
+  dofig(plotdvsd,'big_picture',sim=sim,x=x,y=y,vline=c(-d.crit1,d.crit1),xlim=c(-2,2),
         title=title,cex.main=1);
   ## figure 2
-  title=title_ovrfx('Sharp boundary between nonsignificant and significant p-values');
-  sim.zoom_in=subset(sim,subset=near(d.sdz,d.crit1,0.05));
-  dofig(plotdvsd,'zoom_in',sim=sim.zoom_in,x=x,y=y,d.crit=d.crit1,d.pop=d.fig1,legend=F,
-        vline=d.crit1,vlab=F,
-        title=title,cex.main=1)
+  title=title_ovrfx('All significant results are beyond 0.3',d=d.fig1,n=n.fig1);
+  sim.zoom=subset(sim,subset=near(d.pop,d.fig1,tol=0.02));
+  dofig(plotdvsd,'zoom',sim=sim.zoom,x=x,y=y,vline=c(-d.crit1,d.fig1,d.crit1),hline=d.fig1,hlab=F,
+        legend=F,title=title,cex.main=1);
   ## figure 3
-  title=title_ovrfx('Critical and average observed effect sizes improve as n increases',n=NULL);
-  ## y=data.frame(
-  ##   d.crit,d.mean3=d.mean3[,'meand'],d.mean5=d.mean5[,'meand'],d.mean7=d.mean7[,'meand']);
-  y=data.frame(
-    d.crit,d.mean3=d.mean3[,'meand'],d.mean5=d.mean5[,'meand']);
-  col=setNames(RColorBrewer::brewer.pal(ncol(y),'Set1'),cq(d.crit,0.3,0.5))
+  title=title_ovrfx('Histogram of observed effect size',d=d.fig1,n=n.fig1);
+  sim=get_sim_fixd(n=n.fig1,d=d.fig1);
+  dofig(plothist,'hist',sim=sim,vline=c(-d.crit1,d.fig1,d.crit1),legend.x0=-1.15,
+        title=title,cex.main=1);
+  ## figure 4
+  title=title_ovrfx('Average observed effect size improves as n increases');
+  x=seq(min(n.meand),max(n.meand),by=1);
+  y=do.call(cbind,lapply(meand.fit,function(meand.fit) predict(meand.fit,x)$y));
+  col=setNames(RColorBrewer::brewer.pal(ncol(y),'Set1'),names(meand.byd));
   legend.labels=
-    c('critical effect size',
-      'mean significant effect size for d.pop=0.3','mean significant effect size for d.pop=0.5');
+    c(paste(sep=' ','mean sig effect for true effect',names(meand.byd)),
+      paste(sep=' ','n achieving 1.25x for true effect',names(meand.byd)));
+  col=c(col,col);
+  lty=c(rep('solid',len=3),rep('dotted',3));
+  lwd=2;
   ## dofig(plotm,'dcrit_dmean',x=n,y=y,title=title,cex.main=1,lwd=2,col=col,legend='topright',
   ##       legend.labels=
   ##         c('critical d','mean d for d.pop=0.3','mean d for d.pop=0.5','mean d for d.pop=0.7'),
   ##       xlab='sample size',ylab='effect size',
   ##       vline=round(c(n.fig1,n.crit3,n.crit5)),hline=c(0.3,0.5,0.7));
-  dofig(plotdvsn,'dcrit_dmean',x=n,y=y,title=title,cex.main=1,lwd=2,col=col,
+  dofig(plotmeand,'meand',x=x,y=y,title=title,cex.main=1,lwd=lwd,lty=lty,col=col,        
         legend='topright',legend.labels=legend.labels,
-        d.mean20=c(d.mean3.20,d.mean5.20),
-        n.crit=c(n.crit3,n.crit5),n.over=c(n.over3,n.over5),
+        meand20=meand20,nover=nover,ylim=c(0.3,meand20[3]),
         xlab='sample size',ylab='effect size');
   invisible();
 }
 ## generate title for doc_ovrfx
-title_ovrfx=function(desc=NULL,n=parent(n.fig1)) {
+title_ovrfx=function(desc=NULL,d=NULL,n=NULL) {
   fig=paste(sep='','Figure ',figlabel());
-  paste(collapse="\n",c(fig,desc,if(!is.null(n)) paste_nv(n)));
+  paste(collapse="\n",
+        c(fig,
+          paste(sep='. ',desc,
+                paste(collapse=', ',c(if(!is.null(d))paste_nv(d),if(!is.null(n))paste_nv(n))))));
 }
-## compute n that achieves desired overestimate for given d0 by interpolating meand
-n2over=function(meand,d0,over=1.25) {
-  md=meand[meand$d0==d0,cq(n,over)];
-  ## empirically determined smooth.spline as interp function, and spar=0.1
-  n2over=function(n) predict(with(md,smooth.spline(n,over,spar=0.1)),n)$y;
-  uniroot(function(n) n2over(n)-1.25,interval=c(10,200))$root;
-}
-## plot d.crit, d.mean vs n (figure 3)
-plotdvsn=function(x,y,title,col='black',lty='solid',lwd=1,legend.labels,
-                  d.pop=c(0.3,0.5),d.mean20=NULL,n.crit=NULL,n.over=NULL,...) {
-  col.pop=col[as.character(d.pop)];
-  col.crit=col['d.crit'];
-  ## add legend text for extra lines and adjust line properties
-  legend.labels=c(legend.labels,
-                  'minimum significant sample size',
-                  'sample size that achieves 1.25x 0.3',
-                  'sample size that achieves 1.25x 0.5');
-  col=c(rep(col,len=3),rep(col,len=3));
-  lty=c(rep(lty,len=3),rep('dotted',3));
-  lwd=c(rep(lwd,len=3),rep(2,3));
+
+## plot meand vs n, d (figure 4)
+plotmeand=function(x,y,title,col='black',lty='solid',lwd=1,legend.labels,
+                 meand20=NULL,nover=NULL,...) {
+  param(n.meand,d0.meand);
   ## draw the main plot
-  plotm(x=x,y=y,col=col,lty=lty,lwd=lwd,title=title,legend.labels=legend.labels,...);
+  plotm(x=x,y=y,col=col,lty=lty,lwd=lwd,title=title,legend.labels=legend.labels,smooth=F,
+        ...);
   ## horizontal grid-like lines for d.pop
-  abline(h=d.pop,col=col.pop,lty='dotted',lwd=1);
+  abline(h=d0.meand,col=col,lty='dotted',lwd=1);
   ## horizontal lines for averages with n=20
-  if (!is.null(d.mean20)) {
-    hline(x=20,y=d.mean20,col=col.pop,lty='dotted',lwd=0.5,text=round(d.mean20,digits=2));
-  }
+  ## if (!is.null(meand20)) {
+  ##   hline(x=20,y=meand20,col=col,lty='dotted',lwd=0.5,text=round(meand20,digits=2));
+  ## }
+  ## vertical line for n=20
+  vline(x=20,y0=0,y=1,col='grey',lty='dotted',lwd=1,text=20);
   ## horizontal & vertical lines for overestimates
-  if (!is.null(n.over)) {
-    hline(x=n.over,y=1.25*d.pop,col=col.pop,lty='dotted',lwd=2,text=paste(sep='','1.25x',d.pop));
-    vline(x=n.over,y=1.25*d.pop,col=col.pop,lty='dotted',lwd=2,text=round(n.over));
-  }
-  ## vertical lines for critical crossovers
-  if (!is.null(n.crit)) {
-    vline(x=n.crit,y=d.pop,col=col.crit,lty='dotted',lwd=2,text=round(n.crit));
+  if (!is.null(nover)) {
+    hline(x=nover,y=1.25*d0.meand,col=col,lty='dotted',lwd=2,text=paste(sep='','1.25x',d0.meand));
+    vline(x=nover,y=1.25*d0.meand,col=col,lty='dotted',lwd=2,text=round(nover));
   }
 }
