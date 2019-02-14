4e0e01088bc01b6bf724fb93a290bf1efa3d566f
natgoodman <natg@shore.net>
2019-02-14 14:12:40 -0800

    Support release 1.00 of blog post Remove the 'zoom' figure - old figure 2 Do meand processing on both empirical and theoretical calculations   I inteneded to plot both, buth they're so similar that it's pointess Use theoretical meand calculations for figure 3 and numbers in text Add code for new figure 3 - sampling distributions

--- R/doc_ovrfx.R
+++ R/doc_ovrfx.R
@@ -18,14 +18,18 @@
 ## no sections. only 2 figures
 ## n.fig is sample size for which figures plotted
 doc_ovrfx=function(n.fig1=20,d.fig1=0.3,sect=parent(sect,NULL)) {
-  param(n.meand,d0.meand);
-  meand=get_meand_empi();
-  meand.byd=split(meand,meand$d0);
-  ## make splines that interpolate meand
+  param(n.meand,d.meand);
+  meand.empi=get_meand_empi();
+  meand.theo=get_meand_theo();
+  meand.e.byd=split(meand.empi,meand.empi$d0);
+  meand.t.byd=split(meand.theo,meand.theo$d0);
+  ## make splines that interpolate meand, over
   ##   empirically determined smooth.spline as interp function, and spar=0.3
   spar=0.3;
-  meand.fit=lapply(meand.byd,function(meand) with(meand,smooth.spline(n,meand,spar=spar)))
-  over.fit=lapply(meand.byd,function(meand) with(meand,smooth.spline(n,over,spar=spar)))
+  meand.e.fit=lapply(meand.e.byd,function(meand) with(meand,smooth.spline(n,meand,spar=spar)));
+  ## over.e.fit=lapply(meand.e.byd,function(meand) with(meand,smooth.spline(n,over,spar=spar)));
+  meand.t.fit=lapply(meand.t.byd,function(meand) with(meand,smooth.spline(n,meand,spar=spar)));
+  over.t.fit=lapply(meand.t.byd,function(meand) with(meand,smooth.spline(n,over,spar=spar)));
   ## support statements in text or drawn on figures
   ## do it upfront 'cuz some used in figures
   ## d_crit(n=20)=0.64
@@ -34,14 +38,14 @@ doc_ovrfx=function(n.fig1=20,d.fig1=0.3,sect=parent(sect,NULL)) {
   ##   d=0.3, mean=0.81, over=2.7x
   ##   d=0.5, mean=0.86, over=1.7x
   ##   d=0.7, mean=0.93, over=1.3x
-  meand20=sapply(meand.fit,function(meand.fit) predict(meand.fit,20)$y);
-  over20=sapply(over.fit,function(over.fit) predict(over.fit,20)$y);
+  meand20=sapply(meand.t.fit,function(fit) predict(fit,20)$y);
+  over20=sapply(over.t.fit,function(fit) predict(fit,20)$y);
   ## n that achieves over=1.25x
-  ##   d=0.3 n=121
+  ##   d=0.3 n=122
   ##   d=0.5 n=47
   ##   d=0.7 n=26
-  nover=sapply(over.fit,function(over.fit) {
-    n2over=function(n) predict(over.fit,n)$y;
+  nover=sapply(over.t.fit,function(fit) {
+    n2over=function(n) predict(fit,n)$y;
     uniroot(function(n) n2over(n)-1.25,interval=c(10,200))$root});
   ## save supporting values in table
   names(meand20)=paste(sep='_','meand20',names(meand20));
@@ -58,26 +62,29 @@ doc_ovrfx=function(n.fig1=20,d.fig1=0.3,sect=parent(sect,NULL)) {
   dofig(plotdvsd,'big_picture',sim=sim,x=x,y=y,vline=c(-d.crit1,d.crit1),xlim=c(-2,2),
         title=title,cex.main=1);
   ## figure 2
-  title=title_ovrfx('All significant results are beyond 0.3',d=d.fig1,n=n.fig1);
-  sim.zoom=subset(sim,subset=near(d.pop,d.fig1,tol=0.02));
-  dofig(plotdvsd,'zoom',sim=sim.zoom,x=x,y=y,vline=c(-d.crit1,d.fig1,d.crit1),hline=d.fig1,hlab=F,
-        legend=F,title=title,cex.main=1);
-  ## figure 3
   title=title_ovrfx('Histogram of observed effect size',d=d.fig1,n=n.fig1);
   sim=get_sim_fixd(n=n.fig1,d=d.fig1);
-  dofig(plothist,'hist',sim=sim,vline=c(-d.crit1,d.fig1,d.crit1),legend.x0=-1.15,xlim=c(-1.2,1.5),
-        title=title,cex.main=1);
+  dofig(plothist,'hist',sim=sim,vline=c(-d.crit1,d.fig1,d.crit1),title=title,cex.main=1,
+        legend.x0=-1.85,xlim=c(-2,2));
+  ## figure 3
+  title=title_ovrfx('Sampling distributions show impact of increasing n and d');
+  dofig(plotsmpldist,'smpl_dist',title=title);
   ## figure 4
   title=title_ovrfx('Average observed effect size improves as n increases');
   x=seq(min(n.meand),max(n.meand),by=1);
-  y=do.call(cbind,lapply(meand.fit,function(meand.fit) predict(meand.fit,x)$y));
-  col=setNames(RColorBrewer::brewer.pal(ncol(y),'Set1'),names(meand.byd));
+  y=do.call(cbind,lapply(meand.t.fit,function(fit) predict(fit,x)$y));
+  col=setNames(RColorBrewer::brewer.pal(ncol(y),'Set1'),names(meand.t.byd));
   legend.labels=
-    c(paste(sep=' ','mean sig effect for true effect',names(meand.byd)),
-      paste(sep=' ','n achieving 1.25x for true effect',names(meand.byd)));
-  col=c(col,col);
+    c(paste(sep=' ','mean sig effect for true effect',names(meand.t.byd)),
+      paste(sep=' ','n achieving 1.25x for true effect',names(meand.t.byd)));
+  col=rep(col,2);
   lty=c(rep('solid',len=3),rep('dotted',3));
   lwd=2;
+  ## dofig(plotm,'dcrit_dmean',x=n,y=y,title=title,cex.main=1,lwd=2,col=col,legend='topright',
+  ##       legend.labels=
+  ##         c('critical d','mean d for d.pop=0.3','mean d for d.pop=0.5','mean d for d.pop=0.7'),
+  ##       xlab='sample size',ylab='effect size',
+  ##       vline=round(c(n.fig1,n.crit3,n.crit5)),hline=c(0.3,0.5,0.7));
   dofig(plotmeand,'meand',x=x,y=y,title=title,cex.main=1,lwd=lwd,lty=lty,col=col,        
         legend='topright',legend.labels=legend.labels,
         meand20=meand20,nover=nover,ylim=c(0.3,meand20[3]),
@@ -92,16 +99,44 @@ title_ovrfx=function(desc=NULL,d=NULL,n=NULL) {
           paste(sep='. ',desc,
                 paste(collapse=', ',c(if(!is.null(d))paste_nv(d),if(!is.null(n))paste_nv(n))))));
 }
+## plot sampling distributions (figure 3)
+## all hard-coded because no easy way to automate placement of text
+plotsmpldist=function(title,dlim=c(-1,2)) {
+  ## do n=100 first because it's way taller than the others
+  plotpvsd(n=100,d0=0.3,dlim=dlim,add=F,vline=0.3,title=title);
+  plotpvsd(n=20,d0=0.3,dlim=dlim,add=T);
+  plotpvsd(n=20,d0=0.7,dlim=dlim,vline=0.7,add=T);
+  dtext(n=20,d0=0.3,y=0.5,side='left');
+  dtext(n=20,d0=0.7,y=0.5,side='right');
+  dtext(n=100,d0=0.3,y=2.5,side='left');
+}
+## helper function for placing text on sampling distributions (figure 3)
+dtext=function(n,d0,y,label=paste(sep=', ',paste_nv('d',d0),paste_nv('n',n)),side=cq(left,right),
+               cex=0.9,col='grey10') {
+  side=match.arg(side);
+  ## invert d_d2t to find d corresponding to y
+  y2d=function(d) d_d2t(n=n,d=d,d0=d0)-y;
+  if(side=='left') {
+    d=uniroot(y2d,interval=c(-10,d0))$root
+    label=paste(sep='',label,' ');
+    adj=c(1,0);
+  } else {
+    d=uniroot(y2d,interval=c(d0,10))$root;
+    label=paste(sep='',' ',label);
+    adj=c(0,0);
+  }
+  text(d,y,label,cex=cex,col=col,adj=adj);
+}
 
 ## plot meand vs n, d (figure 4)
 plotmeand=function(x,y,title,col='black',lty='solid',lwd=1,legend.labels,
                  meand20=NULL,nover=NULL,...) {
-  param(n.meand,d0.meand);
+  param(n.meand,d.meand);
   ## draw the main plot
   plotm(x=x,y=y,col=col,lty=lty,lwd=lwd,title=title,legend.labels=legend.labels,smooth=F,
         ...);
   ## horizontal grid-like lines for d.pop
-  abline(h=d0.meand,col=col,lty='dotted',lwd=1);
+  abline(h=d.meand,col=col,lty='dotted',lwd=1);
   ## horizontal lines for averages with n=20
   ## if (!is.null(meand20)) {
   ##   hline(x=20,y=meand20,col=col,lty='dotted',lwd=0.5,text=round(meand20,digits=2));
@@ -110,7 +145,7 @@ plotmeand=function(x,y,title,col='black',lty='solid',lwd=1,legend.labels,
   vline(x=20,y0=0,y=1,col='grey',lty='dotted',lwd=1,text=20);
   ## horizontal & vertical lines for overestimates
   if (!is.null(nover)) {
-    hline(x=nover,y=1.25*d0.meand,col=col,lty='dotted',lwd=2,text=paste(sep='','1.25x',d0.meand));
-    vline(x=nover,y=1.25*d0.meand,col=col,lty='dotted',lwd=2,text=round(nover));
+    hline(x=nover,y=1.25*d.meand,col=col,lty='dotted',lwd=2,text=paste(sep='','1.25x',d.meand));
+    vline(x=nover,y=1.25*d.meand,col=col,lty='dotted',lwd=2,text=round(nover));
   }
 }
