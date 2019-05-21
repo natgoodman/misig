78f145137f91ed302d1bd2ca246a9bf7a888ce32
natgoodman <natg@shore.net>
2019-05-21 11:59:31 -0700

    Replace inline computation of pval and ci tables with call to new functions in R/dat.R

--- R/dat_ovrht.R
+++ R/dat_ovrht.R
@@ -21,17 +21,8 @@ dat_ovrht=function() {
   param(n.hetd,m.hetd,d.hetd,sd.hetd);
   dosim_hetd(n.hetd,m.hetd,d.hetd,sd.hetd);
   ## generate pval and ci tables
-  n=seq(20,400,by=10);
-  sd.het=c(0,0.05,0.1,0.2,0.4);
-  cases=expand.grid(n=n,sd.het=sd.het);
-  pval=with(cases,data.frame(n,sd.het,pval=d2htpval(n=n,sd.het=sd.het,d=d_crit(n))));
-  pval$infl=round(digits=2,pval$pval/0.05)
-  pval$pval=round(digits=2,pval$pval)
-  save_data(pval);
-  if (verbose) print('>>> ci');
-  ci=with(cases,data.frame(n,sd.het,ci=ci_d2ht(n=n,sd.het=sd.het,d=0)[2,]));
-  ci$infl=round(digits=2,ci$ci/ci_d2t(n=cases$n,d=0)[2,]);
-  ci$ci=round(digits=2,ci$ci);
-  save_data(ci);
+  param(n.ovrht,d.ovrht,sd.ovrht);
+  dopval_d2ht(n.ovrht,sd.ovrht);
+  doci_d2ht(n.ovrht,sd.ovrht,d.ovrht);
   invisible();
 }
