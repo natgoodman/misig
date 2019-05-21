61297ac6f27d1e1f6fb7032bd2869a8eac87d664
natgoodman <natg@shore.net>
2019-05-21 12:12:29 -0700

    Change source statements to reflect that mndht document renamed to ovrhtsupp

--- R/run.R
+++ R/run.R
@@ -18,12 +18,12 @@
 source('R/util.R');
 source('R/datman.R');
 source('R/dat.R');
-source('R/dat_mndht.R');
+source('R/dat_ovrhtsupp.R');
 source('R/dat_ovrfx.R');
 source('R/dat_ovrht.R');
 source('R/dat_readme.R');
 source('R/doc.R');
-source('R/doc_mndht.R');
+source('R/doc_ovrhtsupp.R');
 source('R/doc_ovrfx.R');
 source('R/doc_ovrht.R');
 source('R/doc_readme.R');
