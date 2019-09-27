f7f343869aa2ba329c6f42eef5b78c144f5440c6
natgoodman <natg@shore.net>
2019-09-27 11:27:52 -0700

    Fix bug in 'cleanq' function. Add 'clean_type' to corectly do what 'cleanq' was   trying to do Add 'm1' param to control inner-loop of simulation Add params for multiple figures per device Add params for new documents Improve some param names Move generation of d.rand from 'init' to 'dosim_rand'

--- R/init.R
+++ R/init.R
@@ -18,7 +18,7 @@
 ## process parameters and store in param environment.
 ## create output directories if necessary.
 ## supp's are placeholders
-doc.all=cq(readme,readmesupp,ovrfx,ovrht,ovrfxsupp,ovrhtsupp);
+doc.all=cq(readme,readmesupp,ovrfx,ovrfxsupp,ovrht,ovrhtsupp,confi);
 init=function(
   ## doc parameters 
   doc='readme',                             # controls sim defaults, data, figure subdirs
@@ -32,24 +32,22 @@ init=function(
   d.gen=switch(docx,                        # function to generate population effect sizes
                readme=runif,readmesupp=runif,ovrfx=runif),
   d.args=switch(docx,                       # arguments passed to d.gen
-                readme=list(n=m.rand,min=-3,max=3),
-                readmesupp=list(n=m.rand,min=-3,max=3),
-                ovrfx=list(n=m.rand,min=-3,max=3)),
-  d.rand=switch(docx,                       # population effect sizes. call generating function
-                readme=do.call(d.gen,d.args),
-                readmesupp=do.call(d.gen,d.args),
-                ovrfx=do.call(d.gen,d.args)),
+                readme=list(min=-3,max=3),
+                readmesupp=list(min=-3,max=3),
+                ovrfx=list(min=-3,max=3)),
   ## fixed-d 
   n.fixd=switch(docx,                       # sample sizes
                 readme=seq(20,100,by=20),
                 readmesupp=seq(20,100,by=20),
-                ovrfx=seq(20,200,by=20)), 
+                ovrfx=seq(20,200,by=20),
+                confi=c(20,60,100,200)), 
   m.fixd=switch(docx,                       # number of studies per d
-                readme=1e3,readmesupp=1e3,ovrfx=1e4),
+                readme=1e3,readmesupp=1e3,ovrfx=1e4,confi=1e6),
   d.fixd=switch(docx,                       # population effect sizes
                 readme=c(0.2,0.5,0.8),      # Cohen's small, medium, large
                 readmesupp=c(0,0.2,0.5,0.8),# supp needs 0 to compute pvals
-                ovrfx=c(0.3,0.5,0.7)),
+                ovrfx=c(0.3,0.5,0.7),
+                confi=c(0.3,0.5,0.7)),
   ## het-d
   n.hetd=switch(docx,                       # sample sizes
                 readme=c(20,seq(100,400,by=100)),readmesupp=c(20,seq(100,400,by=100)),
@@ -68,28 +66,37 @@ init=function(
   n.ovrht=switch(docx,                       # sample sizes
                  ovrht=seq(20,400,by=10)),
   d.ovrht=switch(docx,ovrht=0),             # centers of het pop effect sizes (ci only)
-  sd.ovrht=switch(docx,                      # standard deviation of het pop distribution
+  sd.ovrht=switch(docx,                     # standard deviation of het pop distribution
                   ovrht=c(0,0.05,0.1,0.2,0.4)),
-  ## ci downsample (ovrhtsupp) - downsample sim results when computing ci, else too slow
-  m.ci=switch(docx,ovrhtsupp=1e3),
+  ## sig levels for pval tables
+  sig.dat=switch(docx,readme=0.05,readmesupp=5*10^(-4:-1),
+                 ovrfxsupp=5*10^(-4:-1),
+                 ovrht=.05,ovrhtsupp=5*10^(-4:-1)),
+  ## ci downsample - downsample sim results when computing ci, else too slow
+  m.ci=switch(docx,readmesupp=1e3,ovrfxsupp=1e3,ovrhtsupp=1e3),
+  ## low level operation of dosim
+  m1=switch(docx,readme=1e2,1e4),           # number of inner-loop iterations
   ## data generation function
   datfun=get(paste(sep='_','dat',docx)),
   ## analysis parameters
-  sig.level=0.05,                   # significance level
-  conf.level=0.95,                  # for confidence intervals
-
+  sig.level=0.05,                    # significance level
+  conf.level=switch(docx,            # confidence levels
+                   confi=seq(0.05,0.95,by=0.05),
+                   0.95),                  
   ## data directories
   datadir=dirname('data',docx,run.id),       # directory for data files
   sim.rand.dir=dirname(datadir,'sim.rand'),  # directory for sim rand files
   sim.fixd.dir=dirname(datadir,'sim.fixd'),  # directory for sim fixd files
   sim.hetd.dir=dirname(datadir,'sim.hetd'),  # directory for sim hetd files
-  outdir=c(datadir,                          # output dirs needed by doc. all need datadir
+  tmpdir=dirname(datadir,'tmp'),             # directory for inner loop sim files
+  outdir=c(datadir,tmpdir,                   # output dirs doc nneds. all need datadir, tmpdir
            switch(docx,
                   readme=c(sim.rand.dir,sim.fixd.dir,sim.hetd.dir),
                   readmesupp=c(sim.rand.dir,sim.fixd.dir,sim.hetd.dir),
                   ovrfx=c(sim.rand.dir,sim.fixd.dir),
                   ovrht=c(sim.hetd.dir),
-                  ovrhtsupp=c(sim.hetd.dir))),
+                  ovrhtsupp=c(sim.hetd.dir),
+                  confi=c(sim.fixd.dir))),
   
   ## NG 18-10-18: figdir, tbldir moved to init_doc
   ## figdir=dirname('figure',docx,mdir), # directory for figures. default eg, figure/repwr/m=1e4
@@ -102,43 +109,47 @@ init=function(
                                  #   NA means save unless file exists
                                  #   T, F mean always or never save
   save.sim=save,                 # save simulations (RData format)
-  save.meand=save,               # save mean effect size results (RData & txt formats)
   save.data=save,                # save top level data
   save.txt=NA,                   # save results in txt format as well as RData
                                  #   NA means use default rule for type:
                                  #   F for all but top level data
   save.txt.sim=!is.na(save.txt)&save.txt,   # save txt simulations. default F
-  save.txt.meand=is.na(save.txt)|save.txt,  # save txt meand results. default T
-  save.txt.data=is.na(save.txt)|save.txt,   # save txt top level results. default T
+  save.txt.data=is.na(save.txt)|save.txt,    # save txt top level results. default T
                                  #    
   clean=switch(docx,readme=T,F), # remove everything and start fresh
   clean.data=clean,              # remove datadir
   clean.sim=F,                   # clean simulations. default F
-  clean.meand=F,                 # clean mean effect size data. default F
+  clean.top=F,                   # clean top level data. default F
+  clean.type=NULL,               # specific data types to clean. see clean_type
   end=NULL                       # placeholder for last parameter
   ) {
   doc=docx;                      # to avoid confusion later
   ## source doc-specific files
   source_doc(doc);
-  ## extend d.rand to cover m
-  if(!is.null(d.rand)) d.rand=rep(d.rand,len=m.rand);
   ## round various d params to avoid imprecise decimals 
   if(!is.null(d.fixd)) d.fixd=round(d.fixd,digits=5);
   if(!is.null(d.hetd)) d.hetd=round(d.hetd,digits=5);
   ## assign parameters to param environment
   ## do it before calling any functions that rely on params
   init_param();
-  ## clean and create output directories as needed
-  if (clean.data) unlink(datadir,recursive=T);
-  ## outdir=c(datadir,sim.rand.dir,sim.fixd.dir);
-  ## create data subdirectories. nop if already exist
-  sapply(outdir,function(dir) dir.create(dir,recursive=TRUE,showWarnings=FALSE));
+  ## clean and create directories as needed
+  if (clean.data) unlink(datadir,recursive=TRUE)
+  else {
+    if (clean.top) {
+      ## adapted from stackoverflow.com/questions/22069095. Thx!
+      paths=list.files(datadir,full.names=TRUE);
+      unlink(paths[!file.info(paths)$isdir]);
+    }
+    if (clean.sim) unlink(simdir,recursive=TRUE);
+  }
   ## clean specific types if desired.
   if (clean.sim) {
     sapply(c(sim.rand.dir,sim.fixd.dir,sim.het.dir),
            function(dir) if (!is.null(dir)) unlink(dir,recursive=T));
   }
-  if (clean.meand) cleanq(meand);
+  sapply(clean.type,clean_type);
+  ## create data subdirectories. nop if already exist
+  sapply(outdir,function(dir) dir.create(dir,recursive=TRUE,showWarnings=FALSE));
   invisible();
 }
 ## initialize doc parameters
@@ -160,6 +171,13 @@ init_doc=function(
   figsfx=outsfx,
   fignum=1,
   figblk=NULL,                  # index into figsfx if in figure block
+  ## for multiple plots per device (page)
+  figdev=FALSE,                           # TRUE when multiple plots per device active
+  figdev.num=1,                           # device (page) number. to construct filenames
+  figdev.pfx='dev',                       # prefix for device filenames
+  figdev.screen=NA,                       # R dev number for plot to screen
+  figdev.file=NA,                         # R dev number for plot to file
+  fig.file=NULL,                          # figure filename - for plotting to screen and file
   ## tables
   tblpfx=outpfx,
   tblsfx=outsfx,
@@ -205,15 +223,20 @@ init_doc=function(
 }
 
 ## clean specific data type. deletes directory, and any top level files
-cleanq=function(what,cleandir=T) {
-  what=as.character(pryr::subs(what));
+clean_type=function(what,cleandir=T) {
+  param(datadir);
   ## delete top level files if exist
-  unlink(filename(datadir,list.files(datadir,pattern=paste(sep='','^',what,'\\.'))));
+  files=list.files(datadir,full.names=T,pattern=paste(sep='','^',what,'\\.(RData|txt)'));
+  unlink(files);
   if (cleandir) {
     whatdir=paste(sep='',what,'dir');
     ## delete directory if exists
     if (exists(whatdir,envir=param.env)) unlink(get(whatdir,envir=param.env),recursive=T);
   }
+}
+cleanq=function(what,cleandir=T) {
+  what=as.character(pryr::subs(what));
+  clean_type(what,cleandir);
 }
 ## setup pval colors. adapted from repwr/R/plot.R/heat_setup
 init_pvcol=function() {
