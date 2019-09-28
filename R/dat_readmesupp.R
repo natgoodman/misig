e52cb47ee1ab9915e9b20f0bd37d2a40d4f598cd
natgoodman <natg@shore.net>
2019-09-28 03:50:20 -0700

    Use new params for rand sim: d.gen, d.args (to generate d.rand inside dosim_rand)     rather than generating d.rand in init     Needed for new simulation design which breaks work into pieces Add call to 'dopval' Re-ordered some calls for stylistic consistency

--- R/dat_readmesupp.R
+++ R/dat_readmesupp.R
@@ -15,39 +15,39 @@
 ## ---- Data Generation for README supplement ----
 dat_readmesupp=function(...) {
   ## do all types of simulations
+  param(n.rand,m.rand,d.gen,d.args);
+  dosim_rand(n.rand,m.rand,d.gen,d.args);
   param(n.fixd,m.fixd,d.fixd);
   dosim_fixd(n.fixd,m.fixd,d.fixd);
-  param(n.rand,m.rand,d.rand);
-  dosim_rand(n.rand,m.rand,d.rand);
   param(n.hetd,m.hetd,d.hetd,sd.hetd);
   dosim_hetd(n.hetd,m.hetd,d.hetd,sd.hetd);
 
   ## run all dat.R data generation functions
   ## compute four statistics - meand, power, pval, ci
-  ##   on five data sources - fixd, rand, hetd, d2t, d2ht
+  ##   on five data sources - fixd, rand hetd, d2t, d2ht
+  ## NOTE rand not used for power, pval since each instance has different d.pop
   #### meand
-  domeand_fixd(n.fixd,d.fixd)
+  domeand_fixd(n.fixd,d.fixd);
   domeand_rand(n.rand);
-  domeand_hetd(n.hetd,d.hetd,sd.hetd);
   domeand_d2t(n.fixd,d.fixd);
+  domeand_hetd(n.hetd,d.hetd,sd.hetd);
   domeand_d2ht(n.hetd,d.hetd,sd.hetd);
   #### power
   dopower_fixd(n.fixd,d.fixd);
-  ## dopower_rand(n); # makes no sense - each instance has different d.pop
-  dopower_hetd(n.hetd,d.hetd,sd.hetd);
   dopower_d2t(n.fixd,d.fixd);
+  dopower_hetd(n.hetd,d.hetd,sd.hetd);
   dopower_d2ht(n.hetd,d.hetd,sd.hetd);
-  #### pval
-  dopval_fixd(n.fixd);
-  ## dopval_rand(n); # makes no sense - need many instances with d.pop=0
-  dopval_hetd(n.hetd,sd.hetd);
-  dopval_d2t(n.fixd);
-  dopval_d2ht(n.hetd,sd.hetd);
+#### pval
+  param(sig.dat);
+  dopval_fixd(n.fixd,sig.dat);
+  dopval_d2t(n.fixd,sig.dat);
+  dopval_hetd(n.hetd,sd.hetd,sig.dat);
+  dopval_d2ht(n.hetd,sd.hetd,sig.dat);
   #### ci
   doci_fixd(n.fixd,d.fixd);
   doci_rand(n.rand);
-  doci_hetd(n.hetd,d.hetd,sd.hetd);
   doci_d2t(n.fixd,d.fixd);
+  doci_hetd(n.hetd,d.hetd,sd.hetd);
   doci_d2ht(n.hetd,d.hetd,sd.hetd);
   
   invisible();
